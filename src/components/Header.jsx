e2fc16a22335fe49cde7465b7d48226102a7d5c2
Denis_Davidov <redman1922svap@mail.ru>
2022-11-04 22:01:00 +0300

    add css modules
    add css modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://papik.pro/uploads/posts/2021-11/thumbs/1636199989_48-papik-pro-p-lotos-logotip-foto-53.jpg'></img>
+    </header>
+}
+
+export default Header;
